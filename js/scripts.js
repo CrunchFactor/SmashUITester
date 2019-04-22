9d58db4b01313c4c1a71d73f199ffcfced30b4c8
Kyle Gibson-Fowlie <k.gibsonfowlie015@mybvc.ca>
2019-04-22 09:03:55 -0600

    v

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ const APIAmenEndpoint = "";
 const APIListingEndpoint = "";
                 
 
-function getAmenitiesFetch(){
+function getKyleFetch(){
     
     let ERRORHANDLER;  
     let APIRequest = {};  
@@ -33,62 +33,3 @@ function handleErrors(response) {
     if (!response.ok) throw Error(response.statusText);
     return response;
 }
-
-function getListingsFetch(){
-    
-    let ERRORHANDLER;  
-    let APIRequest= {};  
-                
-    
-    return fetch(APIListingEndpoint,{ method:'POST', 
-                                          body:JSON.stringify(APIRequest) 
-                                        })
-    .then(handleErrors)
-    .then((response) => response.json())
-    .then((Reply) => Reply.Listings)
-    .then(response => {
-                            response.forEach(listing => {
-                        
-                                let splitList = listing.Amenities.split(', ');    
-                                 
-                            
-                                  AmenSplitListHOLD =  splitList.map(list => {
-                                        
-                                            AmenitiesObject.forEach(Amen => {
-                                    
-                                                if(list == Amen.AmenityId.toString() ){
-                                            
-                                                    list = Amen.AmenityName;
-                                        
-                                                }
-                                                
-                                            })
-                                        return list;
-                                    })
-                                listing.Amenities = AmenSplitListHOLD
-                            
-                                return listing;
-                                
-                            })
-                        
-                            return response;
-                        }
-                       )
-    .catch(ERRORHANDLER => {
-        console.error("Catch --> "); console.error(ERRORHANDLER);
-        /*
-
-            This Parses the JSON into Javascript Objects
-            Then Console logs it.
-
-        */
-        let jsonParse = JSON.parse(PROMISEHANDLER);
-
-        console.log(jsonParse);
-        }
-    );
-}
-
-
-
-
